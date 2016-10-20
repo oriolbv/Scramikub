c11ae6e60b00bf007d3f6eb2b92fd23cbc18628e
oriolbv <oriol.burgaya@est.fib.upc.edu>
2016-10-20 22:01:37 +0200

    ngDraggable added

--- www/js/controllers/GameController.js
+++ www/js/controllers/GameController.js
@@ -21,30 +21,30 @@ angular.module('starter.controllers')
         [{value:"0"}, {value:"0"}, {value:"0"} , {value:"0"}, {value:"0"} , {value:"0"}, {value:"0"} , {value:"0"},{value:"0"}, {value:"0"}, {value:"0"} , {value:"0"}, {value:"0"} , {value:"0"}, {value:"0"}],
     ];
 
-    $scope.chips = [{number:"1", color:"red"},{number:"2", color:"blue"},{number:"3", color:"blue"}];
+    // $scope.chips = [{number:"1", color:"red"},{number:"2", color:"blue"},{number:"3", color:"blue"}];
 
-    var zoom, currentpos;
-    $scope.stopIonicScroll = function(el) {
-        // currentpos = {
-        //     x: el.x,
-        //     y: el.y
-        // };
-        // zoom = $ionicScrollDelegate.$getByHandle('plan').getScrollView().__zoomLevel;
-        // $ionicScrollDelegate.$getByHandle('plan').getScrollView().options.scrollingY = false;
-        // $ionicScrollDelegate.$getByHandle('plan').getScrollView().options.scrollingX = false;
-        //$ionicScrollDelegate.$getByHandle('plan').freezeScroll(true);
-        $ionicScrollDelegate.freezeScroll(true);
-    };
+    $scope.draggableObjects = [{name:"1", color:"red"},{name:"2", color:"blue"},{name:"3", color:"blue"}];
 
-    $scope.startIonicScroll = function() {
-        // $ionicScrollDelegate.$getByHandle('plan').getScrollView().options.scrollingY = true;
-        // $ionicScrollDelegate.$getByHandle('plan').getScrollView().options.scrollingX = true;
-        $ionicScrollDelegate.freezeScroll(false);
-    };
+    $scope.droppedObjects1 = [];
+    $scope.droppedObjects2 = [];
+    $scope.centerAnchor = true;
 
-    $scope.dragging = function($event, element) {
-        // element.x = currentpos.x + Math.round($event.gesture.deltaX / zoom);
-        // element.y = currentpos.y + Math.round($event.gesture.deltaY / zoom);
-    };
+
+    $scope.onDropComplete1 = function (data, evt) {
+        var index = $scope.droppedObjects1.indexOf(data);
+        if (index == -1)
+            $scope.droppedObjects1.push(data);
+    }
+    $scope.onDragSuccess1 = function (data, evt) {
+        console.log("133", "$scope", "onDragSuccess1", "", evt);
+        var index = $scope.droppedObjects1.indexOf(data);
+        if (index > -1) {
+            $scope.droppedObjects1.splice(index, 1);
+        }
+    }
+
+    var inArray = function (array, obj) {
+        var index = array.indexOf(obj);
+    }
 
 });
