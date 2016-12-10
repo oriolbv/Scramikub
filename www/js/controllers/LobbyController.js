d80de5b86691d67558707c60a7648446bd960f2c
oriolbv <oriol.burgaya@est.fib.upc.edu>
2016-12-10 13:19:51 +0100

    SCRKUB-23 Games organized in two different types

--- www/js/controllers/LobbyController.js
+++ www/js/controllers/LobbyController.js
@@ -5,6 +5,8 @@ angular.module('starter.controllers')
     $scope.userConnected = userConnected.auth.token;
     
     $scope.games = Games;
+    $scope.gamesToPlay = [];
+    $scope.gamesToWait = [];
 
     $scope.games.$loaded().then(function (games) {
         console.log($scope.games);
@@ -15,9 +17,12 @@ angular.module('starter.controllers')
                 if (game.players[j] == userConnected.password.email) {
                     if (game.players[game.userTurn] == userConnected.password.email) {
                         game.gameState = "It's your turn! Click to play!"
+                        $scope.gamesToPlay.push(game);
+
                     }
                     else {
                         game.gameState = "It's not your turn. Waiting for other users..."
+                        $scope.gamesToWait.push(game);
                     }
                     auxGames.push(game);
                 }
